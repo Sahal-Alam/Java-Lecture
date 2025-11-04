e12838e8e0684c2c79b89bd2c6e5eb147a8e7fae
Sahal-Alam <117425323+Sahal-Alam@users.noreply.github.com>
2025-11-04 09:18:56 +0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+    static void main(String[] args) {
+        int myNumber = 654;
+        System.out.println(myNumber);
+
+        int theNumber;
+        theNumber = 734;
+        System.out.println(theNumber);
+
+        float myFloat = 5;
+        System.out.println(myFloat);
+
+        double Double = 5.231233;
+        System.out.println(Double);
+
+        String myWord = "Good Morning";
+        System.out.println(myWord);
+
+        boolean student = true;
+        System.out.println(student);
+
+        char myCharacter;
+        myCharacter = 'a';
+        System.out.println(myCharacter);
+
+    }
+}
