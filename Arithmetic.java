cacf0833dd04431a8d0857ce231bf78e36307c6f
Sahal-Alam <117425323+Sahal-Alam@users.noreply.github.com>
2025-11-06 09:11:49 +0600

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,20 @@
+public class Arithmetic {
+    static void main() {
+        int a = 8;
+        int b = 5;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b +"\n");
+
+        double x = 8;
+        double y = 5;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+}
