f37dddb77a4f75619de8d0417886fc4bc8d3905a
Sahal-Alam <117425323+Sahal-Alam@users.noreply.github.com>
2025-11-07 23:48:10 +0600

    Add files via upload

---
+++ OrderOfOperation.java
@@ -0,0 +1,11 @@
+public class OrderOfOperation {
+    static void main() {
+        System.out.println(8-3*3);
+
+        System.out.println(9/(3/3));
+
+        System.out.println(9/3*2/6);
+
+        System.out.println(8-5+7-1);
+    }
+}
---
+++ UnaryOperators.java
@@ -0,0 +1,31 @@
+public class UnaryOperators {
+    static void main(String[] args) {
+        int u = 10;
+        int v = -u;
+        System.out.println(v);
+
+        int a = 5;
+        a = a+1;
+        System.out.println(a);
+        a +=1;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+        ++a;
+        System.out.println(a);
+
+        System.out.println("Watching increment");
+        int p =5;
+        System.out.println(p++);
+        System.out.println(p);
+        System.out.println(++p);
+        System.out.println(p);
+
+        int q = 10;
+        System.out.println(--q);
+        System.out.println(q);
+
+        System.out.println(q--);
+        System.out.println(q);
+    }
+}
