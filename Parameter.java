31d01dd7a4503fa971137fec221baed0c77c7481
Sahal-Alam <117425323+Sahal-Alam@users.noreply.github.com>
2025-11-20 08:50:28 +0600

    Add files via upload

---
+++ Parameter.java
@@ -0,0 +1,12 @@
+public class Parameter {
+    static void main(String[] args) {
+        int result = sum(5,10);
+        System.out.println(result);
+
+    }
+    public static int sum(int first , int second){
+        int add = first + second;
+        return add ;
+    }
+
+}
